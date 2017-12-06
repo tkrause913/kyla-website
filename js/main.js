c633c4ec435e27122b7e90d8d1eab4d001faf07a
tkrause913 <32515309+tkrause913@users.noreply.github.com>
2017-12-06 08:50:35 -0500

    init

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+          $("#slider").slider({
+              animate: true,
+              value:1,
+              min: 0,
+              max: 1000,
+              step: 10,
+              slide: function(event, ui) {
+                  update(1,ui.value); //changed
+              }
+          });
+
+          $("#slider2").slider({
+              animate: true,
+              value:0,
+              min: 0,
+              max: 500,
+              step: 1,
+              slide: function(event, ui) {
+                  update(2,ui.value); //changed
+              }
+          });
+
+          //Added, set initial value.
+          $("#amount").val(0);
+          $("#duration").val(0);
+          $("#amount-label").text(0);
+          $("#duration-label").text(0);
+
+          update();
+      });
+
+      function update(slider,val) {
+        var $amount = slider == 1?val:$("#amount").val();
+        var $duration = slider == 2?val:$("#duration").val();
+
+         $total = "$" + ($amount * $duration);
+         $( "#amount" ).val($amount);
+         $( "#amount-label" ).text($amount);
+         $( "#duration" ).val($duration);
+         $( "#duration-label" ).text($duration);
+         $( "#total" ).val($total);
+         $( "#total-label" ).text($total);
+
+         $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$amount+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+         $('#slider2 a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$duration+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+      }
